71d2afc245a0e784058096a4e9eb90a5fa8a5f27
ARBhuiyanSP <arbhuiyan.pits@gmail.com>
2021-02-14 10:58:38 +0600

    first commit

---
+++ inc/home_portfolio.php
@@ -0,0 +1,102 @@
+<div class="sectionWrapper">
+				<div class="portfolio-filterable full">
+						<div class="">
+							<div class="container">
+								<h3 class="center block-head"><span class="main-color">OUR RUNNING PROJECTS </span></h3>
+							</div>
+							<div class="portfolio-items" id="container">
+								<div class="cell-2 seo" data-category="seo">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://saifsworkshop.com/" class="fx link" target="blank"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/transport.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="Transport Management"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/transport.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://saifsworkshop.com/" target="blank" class="project-name">Transport Management</a>
+										<span class="project-options">Corporate</span>
+									</div>
+									</div>
+								</div>
+								<div class="cell-2 develop" data-category="develop">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://175.29.187.218/saif_icd_yard/" target="blank" class="fx link"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/kicd.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="KICD Yard Management"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/kicd.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://175.29.187.218/saif_icd_yard/" target="blank" class="project-name">KICD Yard Management</a>
+										<span class="project-options">KICD</span>
+									</div>
+									</div>
+								</div>
+								<div class="cell-2 seo" data-category="seo">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://45.249.102.75/saif_port_sms/" target="blank" class="fx link"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/sms.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="Port SMS Notification"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/sms.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://45.249.102.75/saif_port_sms/" target="blank" class="project-name">Port SMS Notification</a>
+										<span class="project-options">CTG</span>
+									</div>
+									</div>
+								</div>
+								<div class="cell-2 design" data-category="design">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://45.249.102.75/cted_inv/" target="blank" class="fx link"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/cct.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="Project Title"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/cct.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://45.249.102.75/cted_inv/" target="blank" class="project-name">CTED Store Inventory</a>
+										<span class="project-options">CTG</span>
+									</div>
+									</div>
+								</div>
+								<div class="cell-2 design" data-category="design">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://103.54.36.11/dev/saif_rlp/" target="blank" class="fx link"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/rlp.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="Project Title"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/rlp.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://103.54.36.11/dev/saif_rlp/" target="blank" class="project-name">RLP Management</a>
+										<span class="project-options">GROUP</span>
+									</div>
+									</div>
+								</div>
+								<div class="cell-2 design" data-category="design">
+									<div class="portfolio-item">
+										<div class="img-holder">
+											<div class="img-over">
+												<a href="http://103.54.36.11/cms" target="blank" class="fx link"><b class="fa fa-link"></b></a>
+												<a href="images/portfolio/cms.jpg" class="fx zoom" data-gal="prettyPhoto[pp_gal]" title="Complain Management"><b class="fa fa-search-plus"></b></a>
+											</div>
+											<img alt="" src="images/portfolio/cms.jpg">
+										</div>
+										<div class="name-holder">
+										<a href="http://103.54.36.11/cms" target="blank" class="project-name">Complain Management</a>
+										<span class="project-options">Corporate</span>
+									</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="clearfix"></div>
+					</div>
+				</div>
